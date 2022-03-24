eced34bb6fd1a9777abbae4c8ea234c5b8d7c5ef
noe <826547343@qq.com>
2022-03-24 12:20:18 +0800

    feat: wasm

---
+++ wasm/fibonacci/fib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+clock_t start, end;
+double duration;
+
+int fib(int n)
+{
+  if (n <= 1)
+  {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char **argv)
+{
+  start = clock();
+  fib(40);
+  end = clock();
+  duration = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("%lf", duration);
+  return 0;
+}
